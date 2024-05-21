791eb88b0634fe76c8c5c21e73c92fb2bbaea58f
fengurwang <fengurwang@tencent.com>
2024-05-21 17:25:55 +0800

    add code & example

---
+++ dateCalculation/dateCalculation/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  dateCalculation
+//
+//  Created by fengur on 2024/5/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
